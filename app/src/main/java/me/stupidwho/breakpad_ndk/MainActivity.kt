7b2e9bc04e751112fe071b118fa430c2955faf1f
ZengWu <stupidwho@163.com>
2020-11-05 17:20:12 +0800

    初始化Android项目

---
+++ app/src/main/java/me/stupidwho/breakpad_ndk/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.stupidwho.breakpad_ndk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
